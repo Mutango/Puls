4001225b8b307224b19947a92c384fbccb44b9c1
Bogdan <bodya.sw@yandex.ru>
2020-11-12 12:15:53 +0300

    sdf

--- src/js/script.js
+++ src/js/script.js
@@ -104,4 +104,19 @@ $(document).ready(function(){
     validateForm('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function (e) {
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: 'mailer/smart.php',
+        date: $(this).serialize()
+      }).done(function () {
+        $(this).find('input').vall("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn();
+        $('form').trigger('reset');
+      });
+      return false;
+    });
 });
